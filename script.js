a97baa56a33b2221b747675063721e3ce1c1c161
Gunjan Dhawan <51229240+GdhawanG@users.noreply.github.com>
2021-09-01 10:47:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ function fetchData() {
 }
 
 //fetchData();
+
+//Weather Data
+fetch(
+  'https://community-open-weather-map.p.rapidapi.com/weather?q=India&lat=0&lon=0&lang=English&units=metric',
+  {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+      'x-rapidapi-key': '51c31b79f4msh59e4c116ba08549p169b50jsnefc854f74f7b',
+    },
+  }
+)
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
