c3438d43c31a381a70e437c17116243ed8408f17
Gunjan Dhawan <51229240+GdhawanG@users.noreply.github.com>
2021-09-07 15:46:29 +0530

    Update script.js
    changed for-each func

--- script.js
+++ script.js
@@ -16,25 +16,22 @@ let headingEnglish = document.getElementById('headingEnglish');
 let newsGerman = document.getElementById('newsGerman');
 let headingGerman = document.getElementById('headingGerman');
 
-function showEngDiv()
-{
+function showEngDiv() {
   englishNewsDiv.style.display = 'block';
   germanNewsDiv.style.display = 'none';
   language = englishDivBtn.value;
   fetchData(language);
-};
+}
 
-function showGermanDiv()
-{
+function showGermanDiv() {
   germanNewsDiv.style.display = 'block';
   englishNewsDiv.style.display = 'none';
   language = germanDivBtn.value;
   fetchData(language);
-};
+}
 
 // Navbar Functions
-function greetUsers()
-{
+function greetUsers() {
   // Testing if greetMsg is correct as per time
   // let dayTime = new Date(2021, 08, 30, 11, 59, 40).getHours();
   // let dayTime = 'Morning';
@@ -44,69 +41,64 @@ function greetUsers()
     dayTime >= 5 && dayTime < 12
       ? 'Morning'
       : dayTime >= 12 && dayTime < 16
-        ? 'Afternoon'
-        : dayTime >= 16 && dayTime < 22
-          ? 'Evening'
-          : 'Night';
+      ? 'Afternoon'
+      : dayTime >= 16 && dayTime < 22
+      ? 'Evening'
+      : 'Night';
 
   let greetingHTML = document.getElementById('greeting');
   greetingHTML.innerHTML = `Good ${dayTime}  `;
   var img = document.createElement('img');
   img.src = `DayTimeIcons/${dayTime}.png`;
   document.getElementById('greeting').appendChild(img);
-};
+}
 
-function clock()
-{
+function clock() {
   let date = new Date().toLocaleDateString();
   let timeNow = new Date().toLocaleTimeString();
   let secondsClockHTML = document.getElementById('secondsClock');
   secondsClockHTML.innerHTML = `It's ${timeNow} !`;
-};
+}
 
 //Using fetch api
-function fetchData(language)
-{
+function fetchData(language) {
   url = `https://newsapi.org/v2/everything?q=volkswagen&sortBy=relevancy&from=${today}&language=${language}&apiKey=${api_key}`;
   fetch(url)
-    .then(Response =>
-    {
+    .then(Response => {
       return Response.json();
     })
-    .then(data =>
-    {
+    .then(data => {
       let newsHtml = '';
       articles = data.articles;
-      articles.forEach(function (elem, ind)
-      {
+      articles.forEach(function callbackFn(element, index) {
         if (language == 'en') {
           let news = `<div class="card">
-                        <div class="card-header" id="heading${ind}">
+                        <div class="card-header" id="heading${index}">
                             <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${ind}"
-                                aria-expanded="false" aria-controls="collapse${ind}">
-                               ${elem['title']}
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                aria-expanded="false" aria-controls="collapse${index}">
+                               ${element['title']}
                             </button>
                             </h2>
                         </div>
-                        <div id="collapse${ind}" class="collapse" aria-labelledby="heading${ind}" data-parent="#englishNewsAccordion">
-                            <div class="card-body"> ${elem['content']}. <a href="${elem['url']}" target="_blank" >Read more here</a>  </div>
+                        <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#englishNewsAccordion">
+                            <div class="card-body"> ${element['content']}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
                         </div>
                     </div>`;
           newsHtml += news;
           englishNewsAccordion.innerHTML = newsHtml;
         } else {
           let news = `<div class="card">
-                        <div class="card-header" id="heading${ind}">
+                        <div class="card-header" id="heading${index}">
                             <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${ind}"
-                                aria-expanded="false" aria-controls="collapse${ind}">
-                               ${elem['title']}
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                aria-expanded="false" aria-controls="collapse${index}">
+                               ${element['title']}
                             </button>
                             </h2>
                         </div>
-                        <div id="collapse${ind}" class="collapse" aria-labelledby="heading${ind}" data-parent="#germanNewsAccordion">
-                            <div class="card-body"> ${elem['content']}. <a href="${elem['url']}" target="_blank" >Read more here</a>  </div>
+                        <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#germanNewsAccordion">
+                            <div class="card-body"> ${element['content']}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
                         </div>
                     </div>`;
           newsHtml += news;
@@ -114,19 +106,17 @@ function fetchData(language)
         }
       });
     })
-    .catch(err =>
-    {
-      console.error(err);
+    .catch(err => {
+      console.log(err);
     });
-};
+}
 
 setInterval(clock, 1000);
 setInterval(greetUsers, 1000);
 fetchData('en');
 
 //Weather Data
-function fetchWeather()
-{
+function fetchWeather() {
   fetch(
     'https://community-open-weather-map.p.rapidapi.com/weather?q=Pune&lang=English&units=metric&rapidapi-key=51c31b79f4msh59e4c116ba08549p169b50jsnefc854f74f7b'
   )
@@ -135,14 +125,12 @@ function fetchWeather()
       data =>
         (document.getElementById('temp').innerHTML = `${data.main.temp}°C`)
     )
-    .catch(err =>
-    {
-      console.log(err);
+    .catch(err => {
+      console.error(err);
     });
-};
+}
 fetchWeather();
 
-function AutoRefresh(mSec)
-{
+function AutoRefresh(mSec) {
   setTimeout('location.reload(true);', mSec);
-};
+}
